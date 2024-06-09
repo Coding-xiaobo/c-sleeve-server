c9dd474c560bd4096a852cc30e59a8b035f3c7a5
xiaobo <zhaoboqi77@163.com>
2024-06-10 00:10:25 +0800

    @ExceptionHandler中返回Unify消息

--- src/main/java/com/xiaobo/csleeve/core/GlobalExceptionAdvice.java
+++ src/main/java/com/xiaobo/csleeve/core/GlobalExceptionAdvice.java
@@ -3,6 +3,8 @@ package com.xiaobo.csleeve.core;
 import com.xiaobo.csleeve.Exception.http.HttpException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,12 +13,17 @@ import javax.servlet.http.HttpServletRequest;
  */
 @ControllerAdvice
 public class GlobalExceptionAdvice {
+    //处理未知异常
     @ExceptionHandler(value = Exception.class)
-    public void handleException(HttpServletRequest req, Exception e) {
-        System.out.println("hello");
+    @ResponseBody
+    public UnifyResponse handleException(HttpServletRequest req, Exception e) {
+        //未知异常code码统一设置为9999
+        UnifyResponse msg = new UnifyResponse(9999,"服务器异常", "URL");
+        return msg;
     }
-    @ExceptionHandler(HttpException.class)
-    public void handleHttpException(HttpServletRequest req, HttpException e) {
-        System.out.println("hello");
-    }
+    //处理已知异常
+//    @ExceptionHandler(HttpException.class)
+//    public void handleHttpException(HttpServletRequest req, HttpException e) {
+//        System.out.println("hello");
+//    }
 }
