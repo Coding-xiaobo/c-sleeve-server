79f8199e3c14c6760927a155e59100871d7dc56c
xiaobo <zhaoboqi77@163.com>
2024-06-10 21:35:20 +0800

    处理前端请求参数

--- src/main/java/com/xiaobo/csleeve/api/v1/BannerController.java
+++ src/main/java/com/xiaobo/csleeve/api/v1/BannerController.java
@@ -1,14 +1,12 @@
 package com.xiaobo.csleeve.api.v1;
 
 import com.xiaobo.csleeve.Exception.http.ForbiddenException;
-import com.xiaobo.csleeve.Exception.http.HttpException;
-import com.xiaobo.csleeve.Exception.http.NotFoundException;
-import com.xiaobo.csleeve.sample.IConnect;
+import com.xiaobo.csleeve.dto.PersonDTO;
 import com.xiaobo.csleeve.sample.ISkill;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -17,9 +15,11 @@ public class BannerController {
     @Autowired
     private ISkill iSkill;
 
-    @GetMapping("/test")
-    public String test() throws Exception {
+    @PostMapping("/test/{id}")
+    public String test(@PathVariable Integer id, @RequestParam String name, @RequestBody PersonDTO person) throws Exception {
+        System.out.println(id);
+        System.out.println(name);
         iSkill.R();
-        throw new ForbiddenException(10001);
+        throw new ForbiddenException(10000);
     }
 }
