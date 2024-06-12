9f9bc9b0f21c9fda4f639d95834ac5694af63206
xiaobo <zhaoboqi77@163.com>
2024-06-12 23:20:33 +0800

    启动/banner/name get接口的开发

--- src/main/java/com/xiaobo/csleeve/api/v1/BannerController.java
+++ src/main/java/com/xiaobo/csleeve/api/v1/BannerController.java
@@ -13,18 +13,8 @@ import javax.validation.constraints.Max;
 @RequestMapping("/banner")
 @Validated
 public class BannerController {
-    @Autowired
-    private ISkill iSkill;
+    @GetMapping("/name/{name}")
+    public void getByName(@PathVariable String name) {
 
-    @PostMapping("/test/{id}")
-    public PersonDTO test(@PathVariable @Max(value = 10, message = "不可以超过10..") Integer id, @RequestParam String name, @RequestBody @Validated PersonDTO person) throws Exception {
-        System.out.println(id);
-        System.out.println(name);
-        //使用builder就不可以new实例化了
-        //PersonDTO dto1 = new PersonDTO();
-        PersonDTO dto = PersonDTO.builder().name("xiaobo").age(18).build();
-        return dto;
-//        iSkill.R();
-//        throw new ForbiddenException(10000);
     }
 }
