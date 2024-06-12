3c91a96dd56325cc76dafa70f9ee08a08549bda5
xiaobo <zhaoboqi77@163.com>
2024-06-12 22:46:50 +0800

    使用validated注解

--- src/main/java/com/xiaobo/csleeve/api/v1/BannerController.java
+++ src/main/java/com/xiaobo/csleeve/api/v1/BannerController.java
@@ -4,16 +4,20 @@ import com.xiaobo.csleeve.Exception.http.ForbiddenException;
 import com.xiaobo.csleeve.dto.PersonDTO;
 import com.xiaobo.csleeve.sample.ISkill;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Max;
 
 @RestController
 @RequestMapping("/banner")
+@Validated
 public class BannerController {
     @Autowired
     private ISkill iSkill;
 
     @PostMapping("/test/{id}")
-    public PersonDTO test(@PathVariable Integer id, @RequestParam String name, @RequestBody PersonDTO person) throws Exception {
+    public PersonDTO test(@PathVariable @Max(value = 10, message = "不可以超过10..") Integer id, @RequestParam String name, @RequestBody @Validated PersonDTO person) throws Exception {
         System.out.println(id);
         System.out.println(name);
         //使用builder就不可以new实例化了
--- src/main/java/com/xiaobo/csleeve/dto/PersonDTO.java
+++ src/main/java/com/xiaobo/csleeve/dto/PersonDTO.java
@@ -1,11 +1,18 @@
 package com.xiaobo.csleeve.dto;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.Valid;
 
 
 @Builder
 @Getter
 public class PersonDTO {
+    @Length(min = 2, max = 10, message = "姓名长度必须在2到10之间")
     private String name;
     private Integer age;
+
+    private String pwd1;
+    private String pwd2;
 }
