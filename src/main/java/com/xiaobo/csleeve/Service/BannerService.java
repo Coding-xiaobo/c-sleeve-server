125f6da994baf367180987fa89d9b2df634c9cf1
xiaobo <zhaoboqi77@163.com>
2024-06-12 23:24:57 +0800

    业务写在service层

---
+++ src/main/java/com/xiaobo/csleeve/Service/BannerService.java
@@ -0,0 +1,10 @@
+package com.xiaobo.csleeve.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BannerService {
+    public void getByName(String name) {
+
+    }
+}
--- src/main/java/com/xiaobo/csleeve/api/v1/BannerController.java
+++ src/main/java/com/xiaobo/csleeve/api/v1/BannerController.java
@@ -1,20 +1,18 @@
 package com.xiaobo.csleeve.api.v1;
 
-import com.xiaobo.csleeve.Exception.http.ForbiddenException;
-import com.xiaobo.csleeve.dto.PersonDTO;
-import com.xiaobo.csleeve.sample.ISkill;
+import com.xiaobo.csleeve.Service.BannerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.Max;
 
 @RestController
 @RequestMapping("/banner")
 @Validated
 public class BannerController {
+    @Autowired
+    private BannerService bannerService;
     @GetMapping("/name/{name}")
     public void getByName(@PathVariable String name) {
-
+        bannerService.getByName(name);
     }
 }
