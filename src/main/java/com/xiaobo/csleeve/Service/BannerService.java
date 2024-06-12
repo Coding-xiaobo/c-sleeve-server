0da2c999ca439c02225444e6d9c7b442faee98e3
xiaobo <zhaoboqi77@163.com>
2024-06-13 00:21:52 +0800

    创建Banner模型

--- src/main/java/com/xiaobo/csleeve/Service/BannerService.java
+++ src/main/java/com/xiaobo/csleeve/Service/BannerService.java
@@ -2,6 +2,9 @@ package com.xiaobo.csleeve.Service;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * 拿到name之后去数据库查询，把查询的结果返回给controller，controller返回给前端
+ */
 @Service
 public class BannerService {
     public void getByName(String name) {
---
+++ src/main/java/com/xiaobo/csleeve/model/Banner.java
@@ -0,0 +1,9 @@
+package com.xiaobo.csleeve.model;
+
+public class Banner {
+    private long id; //banner的主键标识
+    private String name;
+    private String discription;
+    private String imgPath;
+    private String title;
+}
